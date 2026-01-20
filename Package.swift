0ab9cbd4b80586f89cb08ed7498018e21ec57148
Damian Van de Kauter <44820440+vdkdamian@users.noreply.github.com>
2026-01-20 12:31:37 +0100

    Update Swift tools version to 6.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "swift-async-observer",
     platforms: [.macOS(.v13), .iOS(.v16), .watchOS(.v9), .tvOS(.v16)],
     products: [
-        .library(name: "AsyncObserver", targets: ["AsyncObserver"]),
+        .library(name: "AsyncObserver", targets: ["AsyncObserver"])
     ],
     targets: [
         .target(
@@ -16,6 +16,6 @@ let package = Package(
         .testTarget(
             name: "AsyncObserverTests",
             dependencies: ["AsyncObserver"]
-        ),
+        )
     ]
 )
